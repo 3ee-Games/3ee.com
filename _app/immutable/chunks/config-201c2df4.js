22c8b0ff1661a507a45127f9c7ff9f7774ab0fda
Ryan Sadwick <rsadwick@gmail.com>
2026-08-02 11:58:56 -0400

    production build

--- _app/immutable/chunks/config-201c2df4.js
+++
@@ -1,1 +0,0 @@
-function e(){return{serviceUrl:"https://3ee.dev",websocketUrl:"wss://3ee.dev",analytics:!0}}export{e as g};
---
+++ _app/immutable/chunks/config-a7b21997.js
@@ -0,0 +1,1 @@
+function e(){return{serviceUrl:"https://api.3ee.com",websocketUrl:"wss://api.3ee.com",analytics:!0}}export{e as g};
